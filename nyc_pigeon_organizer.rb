b4b3d826f1d27a70cbda1e372453757281f2746c
Jack Cole <123jack.cole@gmail.com>
2020-01-20 00:06:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,14 @@
 def nyc_pigeon_organizer(data)
   new_pigeon_hash = {}
   
-  
-
+  data.each do |attribute, data|
+    if attribute == :color 
+      data.each do |color, names|
+        names.each |index|
+          new_pigeon_hash[index] = color.
+        end
+      end
+    end
+  end
   p new_pigeon_hash
 end
