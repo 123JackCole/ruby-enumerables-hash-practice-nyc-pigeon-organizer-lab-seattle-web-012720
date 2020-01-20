907d86b4a449ada3644e23aa8e666f824e0c534a
Jack Cole <123jack.cole@gmail.com>
2020-01-20 00:15:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,13 +5,28 @@ def nyc_pigeon_organizer(data)
     if attribute == :color 
       data.each do |color, names|
         names.each do |index|
-          if new_pigeon_hash[index]
+          if new_pigeon_hash[index][:color]
             new_pigeon_hash[index][:color] << color.to_s 
           else
             new_pigeon_hash[index] = {:color => [color.to_s]}
           end
         end
       end
+    end
+#    if attribute == :gender
+#      data.each do |gender, names|
+#        names.each |index|
+#          if new_pigeon_hash[index]
+#            new_pigeon_hash[index][:color] << color.to_s 
+#          else
+#            new_pigeon_hash[index] = {:color => [color.to_s]}
+#          end
+#      end
+#    end
+#    if attribute == :lives
+#      data.each do |location, names|
+        
+#      end
     end
   end
   p new_pigeon_hash
